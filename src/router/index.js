bbb6451038c45a07386cc99dc46fd3007589021f
jianghua.pu <ahuadoreen@sohu.com>
2019-06-21 16:52:13 +0800

    初始化导航菜单

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-import Home from '@/components/Home'
+import Main from '@/components/Home'
+import Dashboard from '@/components/Dashboard'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/login',
+//       name: 'Login',
+//       component: Login
+//     },
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home
+//     }
+//   ]
+// })
+let routes = [{
+  path:'/',
+  component: Main,
+  hidden: true,
+  children: [{
       path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
+      component:Dashboard,
+      name:'首页'
+  }]
+}]
+
+import {
+  SystemRouter
+} from './system'
+
+for (let i in SystemRouter){
+  routes.push(SystemRouter[i])
+}
+
+const router = new Router({
+  routes: routes
 })
+
+export default router;
